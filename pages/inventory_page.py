d0c76a434fb6c46dadf971e6b53f03db9c54cabc
Shomikghosh <shomikghosh2020@gmail.com>
2026-03-15 23:08:12 +0530

    added all inventory content check

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -6,6 +6,11 @@ class InventoryPage:
     def __init__(self,page):
         self.inventory_list_selector = page.locator('[data-test="inventory-item"]')
 
+        self.product_name_selector = page.locator('[data-test="inventory-item-name"]')
+        self.product_description_selector = page.locator('[data-test="inventory-item-desc"]')
+        self.product_item_price_selector = page.locator('[data-test="inventory-item-price"]')
+        self.product_item_add_to_cart = page.get_by_role("button",name="Add to cart")
+
     def login_to_inventory(self,page):
         login = LoginPage(page)
         login.navigate()
--- tests/test_inventory_page.py
+++ tests/test_inventory_page.py
@@ -9,3 +9,13 @@ def test_verify_all_product_count(page: Page):
 
     expect(inventoryPage.inventory_list_selector).to_have_count(6)
     
+def test_verify_all_inventory_content(page: Page):
+
+    inventoryPage = InventoryPage(page) 
+    inventoryPage.login_to_inventory(page)
+
+    for (i) in range(6):
+        expect(inventoryPage.product_name_selector.nth(i)).to_be_visible()
+        expect(inventoryPage.product_description_selector.nth(i)).to_be_visible()
+        expect(inventoryPage.product_item_price_selector.nth(i)).to_be_visible()
+        expect(inventoryPage.product_item_add_to_cart.nth(i)).to_be_visible()
